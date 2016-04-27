af24dc71b1087b7b6ac1a0db94ebbda82ceebbb0
Mike Lang <mikelang3000@gmail.com>
2016-04-27 01:22:10 -0700

    Some real work on IPC stuff to connect master and worker procs

--- pipirc/master.py
+++ pipirc/master.py
@@ -1,10 +1,11 @@
 
 
-class MasterServer(object):
-	def __init__(self):
+class IPCServer(object):
+	def __init__(self, main):
 		self.sock_path = '/tmp/{}.sock'.format(uuid4())
+		self.main = main
 		self.group = Group()
-		self.listener = socket.socket(AF_UNIX, SOCK_DGRAM)
+		self.listener = socket.socket(AF_UNIX, SOCK_STREAM)
 		self.listener.listen(128)
 		self.listener.bind(self.sock_path)
 		self.group.spawn(self.run)
@@ -14,40 +15,108 @@ class MasterServer(object):
 		while True:
 			sock, addr = self.listener.accept()
 			name = uuid4()
-			self.conns[name] = SubConnection(name, sock)
-			self.group.spawn(self.conns[name].run)
+			self.conns[name] = IPCMasterConnection(self, name, sock)
+
+	@property
+	def channels(self):
+		ret = {}
+		for conn in self.conns.values():
+			for host, channel in conn.channels:
+				ret.setdefault(host, set()).add(channel)
+		return ret
 
 
-class SubConnection(object):
-	def __init__(self, name, sock):
+class IPCConnection(GSocketClient):
+	def __init__(self, socket):
+		super(SubConnection, self).__init__()
+		self._socket = socket
+
+	def send(self, type, block=False, **data):
+		"""Send message of given type, with other args.
+		Set 'fd' to an integer fd to send that fd over the wire."""
+		data['type'] = type
+		return super(SubConnection, self).send(data, block=block)
+
+	def _send(self, msg):
+		super(SubConnection, self)._send(msg)
+		if msg.get('fd') is not None:
+			send_fd(self._socket, msg['fd'])
+
+	def _encode(self, msg):
+		return json.dumps(msg) + '\n'
+
+	def _handle(self, msg):
+		msg = json.loads(msg)
+		if 'fd' in msg:
+			msg['fd'] = recv_fd(self._socket)
+		msg_type = msg.pop('type')
+		if msg_type in self._handle_map:
+			self._handle_map[msg_type](**msg)
+
+
+class IPCMasterConnection(IPCConnection):
+	def __init__(self, server, name, socket):
+		super(IPCMasterConnection, self).__init__(socket)
+		self.server = server
 		self.name = name
-		self.socket = sock
-		self.group = Group()
-		self.queue = Queue()
-		self.send(name, {'type': 'init', 'name': name})
+		self.channels = set() # set of (host, channel)
+		self.send('init', name=name)
+		self._handle_map = {
+			'chat message': self._send_chat
+			'close channel': self._close_channel,
+		}
+		self.start()
 
-	def run(self):
-		# TODO spawn both and wait on either to error or exit, error handlng etc
+	def _stop(self):
+		super(IPCMasterConnection, self)._stop()
+		assert self.server.conns.pop(name) is self
+		self.server.main.sync_open_channels()
 
-		# TODO braindump
-		# each conn has associated (host:channel)s
-		# we keep track of channel set based on read commands
-		# we send and recv messages for channel
-		# on sock close, remove all channels for conn
-		# also be able to send fd
+	def open_channel(self, host, channel, pip_fd, **options):
+		"""Send channel info and pip protocol fd for given channel to worker process,
+		assigning the channel to this process."""
+		self.channels.add((host, channel))
+		self.server.main.sync_open_channels()
+		self.send('open channel', host=host, channel=channel, fd=pip_fd, **options)
 
-	def send(self, msg, fd=None):
-		self.queue.append(msg, fd)
+	def _close_channel(self, host, channel):
+		self.channels.remove((host, channel))
+		self.server.main.sync_open_channels()
 
-	def send_loop(self):
-		for msg, fd in self.queue:
-			if fd is not None:
-				# TODO
-			else:
-				content = json.dumps(msg)
-				self.sendall(content)
+	def _send_chat(self, host, channel, text):
+		self.server.main.send_chat(host, channel, text)
 
-	def recv_loop(self):
-		while True:
-			msg = self.socket.recv(4096)
-			msg = json.loads(msg)
+	def recv_chat(self, host, channel, text, sender, sender_rank)
+		self.send('chat message', host=host, channel=channel, text=text, sender=sender, sender_rank=sender_rank)
+
+
+class IPCWorkerConnection(IPCConnection):
+	def __init__(self, sock_path):
+		sock = socket.socket(AF_UNIX, SOCK_STREAM)
+		sock.connect(sock_path)
+		super(IPCWorkerConnection, self).__init__(sock)
+		self.channels = {} # {(host, channel): PippyBot}
+		self._handle_map = {
+			'init': self._init,
+			'open channel': self._open_channel,
+			'chat message': self._recv_chat,
+		}
+		self.start()
+
+	def _init(self, name):
+		self.name = name
+
+	def _open_channel(self, host, channel, fd, **options):
+		pip_sock = socket.fromfd(fd, AF_INET, SOCK_STREAM)
+		self.channels[host, channel] = PippyBot(self, pip_sock, host, channel, options)
+
+	def close_channel(self, host, channel):
+		del self.channels[host, channel]
+		self.send('close channel', host=host, channel=channel)
+
+	def send_chat(self, host, channel, text):
+		self.send('chat message', host=host, channel=channel, text=text)
+
+	def _recv_chat(self, host, channel, text, sender, sender_rank):
+		if (host, channel) in self.channels:
+			self.channels[host, channel].recv_chat(text, sender, sender_rank)
