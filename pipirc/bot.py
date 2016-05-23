f696f315ac425c8d3acaef052106ca3e7352d37f
Mike Lang <mikelang3000@gmail.com>
2016-05-23 02:28:08 -0700

    Some notes and stubs related to bot features

--- pipirc/bot.py
+++ pipirc/bot.py
@@ -6,11 +6,18 @@ class PippyBot(object):
 		self.stream_name = stream_name
 		self.config = stream_config
 		self.logger = (logger or logging.getLogger()).getChild(type(self).__name__).getChild(self.stream_name)
-		self.pippy = ClientConnectionFromSocket(pip_sock)
+		self.pippy = gpippy.Client(sock=pip_sock, on_update=on_pip_update)
+		self._init_features()
 		self.say("Connecting...")
+
+	def _init_features(self):
+		# TODO iterate through loaded features, if enabled then register callback
 
 	def recv_chat(self, text, sender, sender_rank):
 		pass # TODO
+
+	def on_pip_update(self, updates):
+		pass #TODO
 
 	def say(self, text):
 		self.ipc.send_chat(self.stream_name, text)
