bdb42b0e404f83b5484c5796d6b3ee656c10dbf3
Mike Lang <mikelang3000@gmail.com>
2016-06-26 05:12:54 -0700

    Remove debugging logs

--- pipirc/features/__init__.py
+++ pipirc/features/__init__.py
@@ -7,5 +7,4 @@ import os
 for filename in os.listdir(os.path.dirname(__file__)):
 	name, ext = os.path.splitext(filename)
 	if ext == '.py' and name != '__init__':
-		print 'import', name
-		print importlib.import_module('.{}'.format(name), __name__)
+		importlib.import_module('.{}'.format(name), __name__)
